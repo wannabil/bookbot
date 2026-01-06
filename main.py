8710bff39e55d31298f0900996e127a6fd540a55
wannabil <nabil@wannabil.com>
2026-01-06 11:37:52 +0800

    done

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_num_of_words, get_num_of_chars, sort_char_count
 
 def get_book_text(pathfile):
@@ -6,18 +7,22 @@ def get_book_text(pathfile):
         return file_contents
 
 def main():
-    pathfile = ("books/frankenstein.txt")
-    texts = get_book_text(pathfile)
-    num_of_words = get_num_of_words(texts)
-    counts = get_num_of_chars(texts)
-    sorted_char_count = sort_char_count(counts)
+    if len(sys.argv) == 2:
+        pathfile = sys.argv[1]
+        texts = get_book_text(pathfile)
+        num_of_words = get_num_of_words(texts)
+        counts = get_num_of_chars(texts)
+        sorted_char_count = sort_char_count(counts)
 
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {pathfile}...")
-    print("----------- Word Count ----------")
-    print(f"Found {num_of_words} total words")
-    print("--------- Character Count -------")
-    for item in sorted_char_count:
-        print(f"{item['char']}: {item['num']}")
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {pathfile}...")
+        print("----------- Word Count ----------")
+        print(f"Found {num_of_words} total words")
+        print("--------- Character Count -------")
+        for item in sorted_char_count:
+            print(f"{item['char']}: {item['num']}")
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
 main()
